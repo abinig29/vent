b92adb0276c77dc5f4e48ff9f8c357b6b7a515fe
abinig29 <abinig5@gmail.com>
2023-03-29 21:56:05 +0300

    create modal

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import { reactToVent } from "../feature/ventSlice";
 
 let token;
+let user;
 const preUser = localStorage.getItem("user");
 if (preUser) {
   token = JSON.parse(preUser).token;
+  user = JSON.parse(preUser).user;
 }
 const AUTHAPI = axios.create({
   baseURL: "http://localhost:5000/api/v1",
@@ -21,16 +23,31 @@ const API = axios.create({
 });
 
 export const getVents = (page) => API.get(`/vent?page=${page}`);
+
 export const reactToSingleVent = (postId, mood) =>
   AUTHAPI.patch(`/vent/${postId}/${mood}`);
+
 export const saveThought = (postId) =>
   AUTHAPI.patch(`/vent/${postId}/saveThought`);
+
 export const followUnfollow = (friendId) =>
   AUTHAPI.patch(`/user/opration/follow`, { friendId });
+
 export const getSingleVent = (postId) => AUTHAPI.get(`/vent/${postId}`);
+
 export const getComment = (postId) => API.get(`/vent/${postId}/comment`);
+
+export const getUsers = (user) =>
+  AUTHAPI.get(`/user${user.length > 0 && `?search=${user}`}`);
+
 export const postComment = (body) => {
   return AUTHAPI.post(`/comment`, body);
+};
+export const createVent = (body) => {
+  return AUTHAPI.post(`/vent`, body);
+};
+export const getListening = () => {
+  return AUTHAPI.get(`/user/${user._id}/lisetning`);
 };
 
 export const login = (userInfo) => {
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,53 +1,66 @@
 import styled from "@emotion/styled";
 
 import { Box, Typography, CircularProgress } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Nav from "../../component/nav/nav";
 import Posts from "../../component/posts/posts";
 import RightBar from "../../component/rightBar/rightBar";
 import { useSelector, useDispatch } from "react-redux";
-import { getAllVents } from "../../feature/ventSlice";
+import { getAllVents, getListeningVents } from "../../feature/ventSlice";
+import { useInfinite } from "../../customHook/useInfinite";
+import { useNavigate } from "react-router-dom";
 
 const Home = ({ type }) => {
   const { posts, isLoading } = useSelector((state) => state.vent);
   const dispatch = useDispatch();
-  const [page, setPage] = useState(1);
-  const handleScroll = () => {
-    const scrollY = window.scrollY;
-    const visibleHeight = document.documentElement.clientHeight;
-    const totalHeight = document.documentElement.scrollHeight;
-    const scrollHeight = scrollY + visibleHeight;
-
-    if (scrollHeight + 5 >= totalHeight * 0.7) {
-      setPage((pre) => pre + 1);
+  const navigate = useNavigate();
+  const [homePage, setHomePage] = useState(1);
+  const { page } = useInfinite(homePage, setHomePage);
+  useEffect(() => {
+    setHomePage(1);
+    if (type === "all") {
+      dispatch(getAllVents(page));
+    } else if (type == "listning") {
+      dispatch(getListeningVents("li"));
     }
-  };
+  }, [type]);
   useEffect(() => {
-    dispatch(getAllVents(page));
+    if (type === "all") {
+      dispatch(getAllVents(page));
+    }
   }, [page]);
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   const StyledBox = styled(Box)({
     display: "flex",
     justifyContent: "space-between",
+  });
+  const CenterBox = styled(Box)({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   });
 
   return (
     <StyledBox>
       <Box flex={"1.2"}>
         <Nav />
-        <Posts posts={posts} />
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          {isLoading && <CircularProgress size={100} />}
-        </Box>
+        {posts.length ? (
+          <Posts posts={posts} />
+        ) : (
+          !isLoading && (
+            <CenterBox maxWidth={"100%"} height={"90vh"}>
+              <Typography variant="h5" color="text.secondary">
+                No vent for today
+              </Typography>
+            </CenterBox>
+          )
+        )}
+        {isLoading && (
+          <CenterBox height={"90vh"}>
+            <CircularProgress size={100} />
+          </CenterBox>
+        )}
       </Box>
-
       {/* <Posts saved={true} posts={posts} isLoading={isLoading} /> */}
-
       <Box flex={"1"}>
         <RightBar />
       </Box>
